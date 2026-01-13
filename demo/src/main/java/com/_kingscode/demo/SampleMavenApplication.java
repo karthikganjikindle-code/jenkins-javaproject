292f1938f92485ec09467b11583e82fb840aa177
karthikganjikindle-code <karthikganjikindle@gmail.com>
2026-01-14 02:59:59 +0530

    Add files via upload

---
+++ demo/src/main/java/com/_kingscode/demo/SampleMavenApplication.java
@@ -0,0 +1,13 @@
+package com._kingscode.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleMavenApplication.class, args);
+	}
+
+}
